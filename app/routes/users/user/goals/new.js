956ff2bd0b163d46f7c3ddaa0001c0615d4660c1
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-16 20:35:17 -0400

    fixed date stuff, transition to new goal show page

--- app/routes/users/user/goals/new.js
+++ app/routes/users/user/goals/new.js
@@ -13,7 +13,7 @@ export default Ember.Route.extend({
     save: function() {
       let goal = this.modelFor(this.routeName)
       goal.save().then(() => {
-        this.transitionTo('users.user.goals')
+        this.transitionTo('users.user.goals.goal', goal)
       })
     }
   }
