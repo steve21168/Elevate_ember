d48a9878b212e4e4add4ad7d2548ff4491c8963f
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-20 14:33:14 -0400

    lots of styling and random quotes

--- app/components/random-quote.js
+++ app/components/random-quote.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    randomize: function() {
-      var quotes = [{description: 'hi'}, {description: 'yo'}, {description: 'hola'}]
-      return quotes[Math.floor(Math.random() * quotes.length)].description
-    }
-  }
 });
